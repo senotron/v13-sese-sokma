7184341d2f0431e28b0611e33c6d3ba6d0b49766
Glitch (harsh-married-toucan) <none>
2023-03-24 10:21:22 +0000

    🕋🏚 Checkpoint ./mrsnletcode.js:56284628/1181

--- mrsnletcode.js
+++ mrsnletcode.js
@@ -5,8 +5,17 @@ const fs = require('fs');
 client.on("ready",async () => {
 
     console.log("Bot Hizmete Hazır!");
-    client.user.setActivity("MrSn-LetC", {type:"WATCHING"});
+    client.user.setActivity("MrSn-LetCode", {type:"WATCHING"});
     
 });
-//#endregion
 client.login(process.env.token);
+
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+client.on('ready', () => {  //MrSn
+  joinVoiceChannel({ 
+channelId: "1088766869860786187", //MrSn
+guildId: "1055105621327888394",    //MrSn
+adapterCreator: client.guilds.cache.get("1055105621327888394").voiceAdapterCreator
+    }); //MrSn
+}); //MrSn
