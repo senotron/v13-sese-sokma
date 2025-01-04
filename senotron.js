1f397f3a1ae6cb6db95e46d3c5cef08d446b69eb
senotron <105975100+senotron@users.noreply.github.com>
2025-01-04 11:39:52 +0400

    Update and rename mrsnletcode.js to senotron.js

---
+++ senotron.js
@@ -0,0 +1,27 @@
+const { Client } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+require("dotenv").config();
+
+const client = new Client({ intents: 519 });
+
+client.once("ready", async () => {
+  console.log("Bot Hizmete Hazır!");
+  client.user.setActivity("https://youtube.com/@s3notron", { type: "WATCHING" });
+
+  const channelId = "SES KANALI ID";
+  const guildId = "SUNUCU ID";
+  const guild = client.guilds.cache.get(guildId);
+
+  if (guild) {
+    joinVoiceChannel({
+      channelId: channelId,
+      guildId: guildId,
+      adapterCreator: guild.voiceAdapterCreator,
+    });
+    console.log("Ses kanalına bağlandı!");
+  } else {
+    console.error("Sunucu bulunamadı, ses kanalına bağlanılamadı.");
+  }
+});
+
+client.login(process.env.token);
